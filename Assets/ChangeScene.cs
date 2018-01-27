931106a007c951d95d9fb38ec075ab727940eab8
LAPTOP-J93P7H0L\GIO <mirkopacella1998@gmail.com>
2018-01-27 10:47:22 +0100

    Menu iniziale

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+    // Use this for initialization
+    void Start () {
+    }
+    public void Change() {
+        SceneManager.LoadScene("PlatformScene", LoadSceneMode.Single);
+    }
+    public void Quit() {
+        Application.Quit();
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
