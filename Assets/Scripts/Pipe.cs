547213ef8516c854fbbd3a98a0a306a508c68007
Mauro Vanetti <posta@maurovanetti.info>
2018-01-27 12:24:57 +0100

    Tubo rotante

---
+++ Assets/Scripts/Pipe.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour {
+
+    public bool ccw;
+    public SpriteRenderer rotatingArt;
+    public float rotationDuration;
+
+    private bool rotating;
+    private float rotationDegrees;
+    private float DiscreteRotation
+    {
+        get { return (ccw ? +1 : -1) * 90f; }
+    }
+
+	// Use this for initialization
+	void Start () {
+        rotationDegrees = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (rotating)
+        {
+            float deltaDegrees = Time.deltaTime * (DiscreteRotation / rotationDuration);            
+            rotationDegrees = rotationDegrees + deltaDegrees;
+            if (Mathf.Abs(rotationDegrees) >= Mathf.Abs(DiscreteRotation))
+            {
+                transform.Rotate(0, 0, DiscreteRotation);
+                rotationDegrees = 0;
+                rotating = false;
+            }
+            rotatingArt.transform.localRotation = Quaternion.Euler(0, 0, rotationDegrees);
+        }
+	}
+
+    private void OnMouseDown()
+    {
+        rotating = true;        
+    }
+}
