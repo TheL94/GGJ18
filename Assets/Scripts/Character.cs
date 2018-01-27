765e0293255f0cba46aaa86ac40d22465829f860
Luca Garbelli <lu.gbelli@hotmail.it>
2018-01-27 12:30:40 +0100

    - moviemnto character funzionanate

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -52,9 +52,9 @@ public class Character : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
             currentAction = CharacterAction.Jump;
-        else if (Input.GetKey(KeyCode.A))
+        else if (Input.GetKeyDown(KeyCode.A))
             currentAction = CharacterAction.GoLeft;
-        else if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKeyDown(KeyCode.D))
             currentAction = CharacterAction.GoRight;
 
         ChooseAction(currentAction);
