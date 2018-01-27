235cfff1c2a4cd32b260f676038c025b77f39790
Luca Garbelli <lu.gbelli@hotmail.it>
2018-01-27 09:09:36 +0100

    - commit iniziale

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public float MovementSpeed;
+    public float JumpForce;
+
+    Rigidbody2D rigid;
+
+	void Start ()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate ()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+            Move(Vector2.left);
+        if (Input.GetKeyDown(KeyCode.D))
+            Move(Vector2.right);
+        if (Input.GetKeyDown(KeyCode.Space))
+            Jump();        
+    }
+
+    void Move(Vector2 _direction)
+    {
+        //rigid.MovePosition(_direction * MovementSpeed);
+        transform.Translate(_direction * MovementSpeed);
+    }
+
+    void Jump()
+    {
+        rigid.AddForce(Vector2.up * JumpForce, ForceMode2D.Force);
+    }
+}
