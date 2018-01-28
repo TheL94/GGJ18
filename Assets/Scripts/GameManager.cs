ab64e6d3355a4f86934721dc15adc89cfc82b153
Luca Garbelli <lu.gbelli@hotmail.it>
2018-01-28 09:22:38 +0100

    - minor fix

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,6 +14,8 @@ public class GameManager : MonoBehaviour {
             I = this;
         else
             DestroyImmediate(gameObject);
+
+        DontDestroyOnLoad(gameObject);
     }
 
     public void GameOver()
